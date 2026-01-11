4cf8fe8945d9cc26c805c1e381dff517f39d033d
Lawrence Arryl Lopez <160656561+arryllopez@users.noreply.github.com>
2026-01-10 23:51:41 -0500

    simple string and file manipulation

---
+++ Chapter-2/name_cases.py
@@ -0,0 +1,24 @@
+name = "Eric" 
+
+print(f"Hello {name}, would you like to learn some Python today?")
+
+name2 = "Matthew" 
+
+print(name2.lower(), name2.upper(),  name2.title())
+
+print("Albert Einstein once said, 'A person who never made a mistake never tried anything new.'")
+
+famous_person = "Albert Einstein"
+
+print(f"{famous_person} once said, 'A person who never made a mistake never tried anything new.'")
+
+person_name = "   \n\t  John Doe  \n\t   "
+
+print(person_name)
+print(person_name.lstrip())
+print(person_name.rstrip())
+print(person_name.strip())
+
+filename = "example_file.txt" 
+
+print(filename.removesuffix(".txt"))
